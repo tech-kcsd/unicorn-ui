2b4a5afe3dd170bbea4065ca4b8105c502a6b94d
Samat Tokish <57891693+anzeder@users.noreply.github.com>
2022-08-04 16:32:49 +0600

    DashboardHr and headbar  (#2)
    * added dashboard for HR
    
    * created dashboard-hr component
    
    * deleted package-lock.json

---
+++ src/app/dashboard-hr/dashboard-hr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard-hr.component.html',
+  styleUrls: ['./dashboard-hr.component.scss']
+})
+export class DashboardHRComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
